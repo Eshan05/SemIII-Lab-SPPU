8154d34b2cda95ec2cb13ae2b4ef4fe9681a7ab7
Eshan05 <nahareshan05@gmail.com>
2024-08-22 00:15:32 +0530

    Add proper extensions to files

--- DSL/C/C23.cpp
+++ DSL/C/C23.cpp
@@ -1,5 +1,3 @@
-"""
-23. WCP for storing binary number using doubly linked lists. Write functions-
-  a) To compute 1‘s and 2‘s complement
-  b) Add two binary numbers 
-"""
+// 23. WCP for storing binary number using doubly linked lists. Write functions-
+//   a) To compute 1‘s and 2‘s complement
+//   b) Add two binary numbers 
--- DSL/C/C24.cpp
+++ DSL/C/C24.cpp
@@ -1,5 +1,5 @@
-"""
-24. WCP to realize Set using Generalized Liked List (GLL)
-$$A = {a, b, {c, d, e, {}, {f,g}, h, l, {j,k}, l, m}}$$
-Store and print as set notation
-"""
+/**
+ * 24. WCP to realize Set using Generalized Liked List (GLL)
+ * $$A = {a, b, {c, d, e, {}, {f,g}, h, l, {j,k}, l, m}}$$
+ * Store and print as set notation
+*/
