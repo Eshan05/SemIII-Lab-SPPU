af5406c58121f65521a92f8c6543eafc4ab680ed
Eshan05 <nahareshan05@gmail.com>
2024-08-30 20:54:32 +0530

    Complete OOPL

--- OOPL/C6.cpp
+++ OOPL/C6.cpp
@@ -3,3 +3,115 @@
  * Write C++ program using STL for sorting and searching user defined records such as personal records (Name, DOB, Telephone number, ...) using vector container
  * OR
  */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class PersonalRecord {
+	private: 
+		string name;
+		string dob;
+		string phone;
+	public:
+		PersonalRecord(const string& name = "", const string& dob = "", const string& phone = "") : name(name), dob(dob), phone(phone) {}
+		string getName() const { return name; }
+		void setName(const string& name) { this->name = name; }
+		// Use this->name to distinguish from member variable and parameter passed
+		void display() const {
+			cout << "Name: " << name << ", DOB: " << dob << ", Phone: " << phone << endl;
+		}
+
+		// Using const so it won't modify the object on which it's called
+		// Comparision required for sorting
+		bool operator<(const PersonalRecord& other) const {
+			return name < other.name;
+		}
+
+		// Equality required for searching
+		bool operator==(const PersonalRecord& other) const {
+			return name == other.name;
+		}
+};
+
+class PersonalRecordManager {
+	private:
+		vector<PersonalRecord> records;
+	public:
+		void addRecord() {
+			string name, dob, phone;
+			cout << "Enter Name: "; getline(cin, name);
+			cout << "Enter DOB (YYYY-MM-DD): "; getline(cin, dob);
+			cout << "Enter Phone No.: "; getline(cin, phone);
+			records.emplace_back(name,dob,phone);
+			cout << "Record added!\n";
+		}
+
+		void displayRecords() const {
+			if(records.empty()) cout << "No records.\n";
+			else 
+				for(const auto& record : records)
+					record.display();
+		}
+
+		void sortRecords() {
+			sort(records.begin(), records.end());
+			cout << "Records sorted by name.\n";
+		}
+
+		void searchRecord() const {
+			string searchName;
+			cout << "Enter a name to search: ";
+			getline(cin, searchName);
+			PersonalRecord searchRecord(searchName);
+			auto it = find(records.begin(), records.end(), searchRecord);
+			if(it != records.end()) {
+				cout << "Record found:\n";
+				it -> display();
+			} else cout << "Record not found.\n";
+		}
+
+		void deleteRecord() {
+			string deleteName;
+			cout << "Enter name to delete: ";
+			getline(cin, deleteName);
+			for(auto it=records.begin(); it!=records.end(); ++it) {
+				if(it -> getName() == deleteName) {
+					records.erase(it);
+					cout << "Record deleted!\n";
+					return;
+				}
+			}
+			cout << "Record not found.\n";
+		}
+
+		static void printMenu() {
+			cout << "\nMenu:\n";
+			cout << "1. Add Record\n";
+			cout << "2. Display Records\n";
+			cout << "3. Sort Records\n";
+			cout << "4. Search Record\n";
+			cout << "5. Delete Record\n";
+			cout << "6. Exit\n";
+			cout << "Enter your choice: ";
+    }
+};
+
+int main() {
+	PersonalRecordManager manager;
+	int choice;
+	do {
+		PersonalRecordManager::printMenu();
+		cin >> choice;
+		cin.ignore();
+		switch(choice) {
+			case 1: manager.addRecord(); break;
+			case 2: manager.displayRecords(); break;
+			case 3: manager.sortRecords(); break;
+			case 4: manager.searchRecord(); break;
+			case 5: manager.deleteRecord(); break;
+			case 6: cout << "Exiting\n"; break;
+			default: cout << "Invalid choice\n"; break;
+		} 
+	} while(choice!=6);
+	return 0;
+}
--- OOPL/C7.cpp
+++ OOPL/C7.cpp
@@ -2,3 +2,30 @@
  * * Eshan Nahar
  * Write a program in C++ to use map associative container. The keys will be the names of states and the values will be the populations of the states. When the program runs, the user is prompted to type the name of a state. The program then looks in the map, using the state name as an index and returns the population of the state
  */
+
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
+
+int main() {
+	map<string, long> statePopulation;
+	statePopulation["First"] = 39538223;
+	statePopulation["Second"] = 29145505;
+	statePopulation["Third"] = 21538187;
+	statePopulation["Fourth"] = 20201249;
+	statePopulation["Fifth"] = 13002700;
+	statePopulation["Sixth"] = 12812508;
+	statePopulation["Seventh"] = 11799448;
+	statePopulation["Eighth"] = 10711908;
+	statePopulation["Ninth"] = 10439388;
+	statePopulation["Tenth"] = 10077331;
+	
+	string stateName;
+	cout << "Enter name of a state: ";
+	getline(cin, stateName);
+	auto it = statePopulation.find(stateName);
+	if(it != statePopulation.end()) cout << "Population of " << stateName << " is " << it->second << "endl";
+	else cout << "State not present\n";
+	return 0; 
+}
