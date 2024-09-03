396d70340f11b9899c1db052ee12e83c175c70a2
Eshan05 <nahareshan05@gmail.com>
2024-09-03 21:50:17 +0530

    Fix issues in B11.py

--- DSL/B/B11.py
+++ DSL/B/B11.py
@@ -9,12 +9,14 @@ class Searching:
   def __init__(self):
     self.__listLength = 0
     self.__searchList = []
+    self.__originalList = []
 
   def accept(self):
     self.__listLength = int(input("Enter the number of elements in list: "))
     for i in range(self.__listLength):
       t = int(input(f"Enter element a[{i}]: "))
       self.__searchList.append(t)
+      self.__originalList.append(t)
 
   def linear(self, key):
     for i in range(self.__listLength):
@@ -33,24 +35,26 @@ class Searching:
       return i
     return -1
 
-  def insertionSort(self):
-    for i in range(1, self.__listLength):
-      key = self.__searchList[i]
+  def insertionSort(self, arr):
+    for i in range(1, len(arr)):
+      key = arr[i]
       j = i - 1
-      while j >= 0 and key < self.__searchList[j]:
-        self.__searchList[j + 1] = self.__searchList[j]
+      while j >= 0 and key < arr[j]:
+        arr[j + 1] = arr[j]
         j -= 1
-      self.__searchList[j + 1] = key
+      arr[j + 1] = key
 
   def binary(self, key):
+    sorted_list = self.__originalList.copy()
+    self.insertionSort(sorted_list)
     low = 0
-    high = self.__listLength - 1
+    high = sorted_list - 1
     mid = 0
     while low <= high:
       mid = (high + low) // 2
-      if self.__searchList[mid] < key:
+      if sorted_list[mid] < key:
         low = mid + 1
-      elif self.__searchList[mid] > key:
+      elif sorted_list[mid] > key:
         high = mid - 1
       else:
         return mid
@@ -58,7 +62,8 @@ class Searching:
   
   def fibonacci(self, key):
     n = self.__listLength
-    self.insertionSort() 
+    sortedList = self.__originalList.copy()
+    self.insertionSort(sortedList) 
     fibM2, fibM1 = 0, 1
     fibM = fibM2 + fibM1
     while(fibM < n):
@@ -67,20 +72,22 @@ class Searching:
     offset = -1
     while(fibM > 1):
       i = min(offset + fibM2, n-1)
-      if(self.__searchList[i] < key):
+      if(sortedList[i] < key):
         # Key greater: Move third variable one down and reset offset to index
         fibM, fibM1 = fibM1, fibM - fibM1
         fibM2 = fibM - fibM1
         offset = i
-      elif(self.__searchList[i] > key):
+      elif(sortedList[i] > key):
         # Key less than element: Move third variable two down
         fibM, fibM1 = fibM1, fibM1 - fibM2
         fibM2 = fibM - fibM1
       else: # First condition
-        return i
+        # return i
+        return self.__originalList.index(key)
       
-    if(fibM1 and offset < n-1 and self.__searchList[n-1] == key):
-      return offset + 1
+    if(fibM1 and offset < n-1 and sortedList[n-1] == key):
+      # return offset + 1
+      return self.__originalList.index(key)
     return -1
   
   def printList(self):
@@ -120,7 +127,7 @@ if __name__ == "__main__":
     if choice == 4:
       print("Selected Fibonacci Search")
       print("Sorting the array...")
-      key = int(input("Enter the element to be searched:"))
+      key = int(input("Enter the element to be searched: "))
       if obj.fibonacci(key) == -1:
         print("Element not found")
       else:
