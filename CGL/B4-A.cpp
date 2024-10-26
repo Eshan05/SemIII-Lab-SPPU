966534b5487bfd9e959187f65c341fedeaba89ef
Eshan05 <nahareshan05@gmail.com>
2024-10-26 10:59:39 +0530

    Add B4-A and B4-B

--- CGL/B4-A.cpp
+++ CGL/B4-A.cpp
@@ -1,9 +1,125 @@
 // * Eshan Nahar
 /**
- * Write C++ Program to draw 2-D object and perform following basic transformations
- * a) Scaling
- * b) Transformation
- * c) Rotation
- * 
+ * Write C++ Program to draw 2-D object and perform following basic
+ * transformations a) Scaling b) Transformation c) Rotation
+ *
  * Apply the concept of operator overloading
  */
+
+#include <graphics.h>
+#include <math.h>
+
+#include <iostream>
+using namespace std;
+class transform {
+ public:
+  int m, a[20][20], c[20][20];
+  int i, j, k;
+
+ public:
+  void display();
+  void accept();
+  void operator*(float b[20][20]) {
+    for(int i = 0; i < m; i++) {
+      for(int j = 0; j < m; j++) {
+        c[i][j] = 0;
+        for(int k = 0; k < m; k++) { c[i][j] = c[i][j] + (a[i][k] * b[k][j]); }
+      }
+    }
+  }
+};
+
+void transform::display() {
+  int gd, gm;
+  gd = DETECT;
+  initgraph(&gd, &gm, NULL);
+  line((300), 0, (300), 600);
+  line(0, (300), 600, (300));
+  for(i = 0; i < m - 1; i++) {
+    line(300 + a[i][0], 300 - a[i][1], 300 + a[i + 1][0], 300 - a[i + 1][1]);
+  }
+  line(300 + a[0][0], 300 - a[0][1], 300 + a[i][0], 300 - a[i][1]);
+  for(i = 0; i < m - 1; i++) {
+    line(300 + c[i][0], 300 - c[i][1], 300 + c[i + 1][0], 300 - c[i + 1][1]);
+  }
+  line(300 + c[0][0], 300 - c[0][1], 300 + c[i][0], 300 - c[i][1]);
+  delay(5000);
+  closegraph();
+}
+
+void transform::accept() {
+  cout << "\nEnter the number of edges: ";
+  cin >> m;
+  cout << "Enter the coordinates (Format: X Y)" << endl;
+  for(int i = 0; i < m; i++) {
+    cout << (i + 1) << ": ";
+    for(int j = 0; j < 3; j++) {
+      if(j >= 2) {
+        a[i][j] = 1;
+      } else {
+        cin >> a[i][j];
+      }
+    }
+  }
+
+  cout << "\nCoordinates entered:" << endl;
+  for(int i = 0; i < m; i++) {
+    cout << "Vertex " << i + 1 << ": (" << a[i][0] << ", " << a[i][1] << ")"
+         << endl;
+  }
+}
+
+int main() {
+  int ch, tx, ty;
+  float sx, sy;
+  float deg, theta, b[20][20];
+  transform t;
+  t.accept();
+  while(true) {
+    cout << "\n=== Enter your choice "
+            "==\n\n1.Translation\n2.Scaling\n3.Rotation\n4.Exit\n0.Enter new "
+            "coordinates\n"
+         << endl;
+    cin >> ch;
+    switch(ch) {
+      case 1:
+        cout << "\n--- TRANSLATION OPERATION ---\nEnter value for TX TY: ";
+        cin >> tx >> ty;
+        b[0][0] = b[2][2] = b[1][1] = 1;
+        b[0][1] = b[0][2] = b[1][0] = b[1][2] = 0;
+        b[2][0] = tx;
+        b[2][1] = ty;
+        t *b;
+        t.display();
+        break;
+      case 2:
+        cout << "\n*** SCALING OPERATION ***\nEnter value for SX SY: ";
+        cin >> sx >> sy;
+        b[0][0] = sx;
+        b[1][1] = sy;
+        b[0][1] = b[0][2] = b[1][0] = b[1][2] = 0;
+        b[2][0] = b[2][1] = 0;
+        b[2][2] = 1;
+        t *b;
+        t.display();
+        break;
+      case 3:
+        cout << "\n*** ROTATION OPERATION ***\nEnter value for angle: ";
+        cin >> deg;
+        theta = deg * (3.14 / 180);
+        b[0][0] = b[1][1] = cos(theta);
+        b[0][1] = sin(theta);
+        b[1][0] = sin(-theta);
+        b[0][2] = b[1][2] = b[2][0] = b[2][1] = 0;
+        b[2][2] = 1;
+        t *b;
+        t.display();
+        break;
+      case 4: exit(0);
+      case 0: t.accept(); break;
+      default: cout << "\nInvalid choice";
+    }
+  }
+  getch();
+  return 0;
+}
--- CGL/B4-B.cpp
+++ CGL/B4-B.cpp
@@ -1,4 +1,169 @@
 // * Eshan Nahar
 /**
- * Write C++ program to implement translation, rotation and scaling transformations on equilateral triangle and rhombus. Apply the concept of operator overloading
+ * Write C++ program to implement translation, rotation and scaling
+ * transformations on equilateral triangle and rhombus. Apply the concept of
+ * operator overloading
  */
+
+#include <graphics.h>
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class TRT {
+  int x1, y1, x2, y2, x3, y3;
+  int rx1, ry1, rx2, ry2, rx3, ry3, rx4, ry4;
+  void rotatePoint(int &x, int &y, float angle);
+
+ public:
+  // Constructor to initialize coordinates
+  TRT() {
+    // Triangle coordinates
+    x1 = 150;
+    y1 = 50;
+    x2 = 100;
+    y2 = 100;
+    x3 = 200;
+    y3 = 100;
+
+    // Rhombus coordinates (center at (150, 150))
+    rx1 = 150;
+    ry1 = 100; // Top
+    rx2 = 100;
+    ry2 = 150; // Left
+    rx3 = 150;
+    ry3 = 200; // Bottom
+    rx4 = 200;
+    ry4 = 150; // Right
+  }
+
+  void translate();
+  void scale();
+  void rotate();
+  void draw(int shapeType);
+  void drawTriangle();
+  void drawRhombus();
+};
+
+void TRT::translate() {
+  float tx, ty;
+  cout << "Enter TX TY: ";
+  cin >> tx >> ty;
+  x1 += tx;
+  y1 += ty;
+  x2 += tx;
+  y2 += ty;
+  x3 += tx;
+  y3 += ty;
+  rx1 += tx;
+  ry1 += ty;
+  rx2 += tx;
+  ry2 += ty;
+  rx3 += tx;
+  ry3 += ty;
+  rx4 += tx;
+  ry4 += ty;
+}
+
+void TRT::scale() {
+  float sx, sy;
+  cout << "Enter SX SY: ";
+  cin >> sx >> sy;
+  x1 *= sx;
+  y1 *= sy;
+  x2 *= sx;
+  y2 *= sy;
+  x3 *= sx;
+  y3 *= sy;
+  rx1 *= sx;
+  ry1 *= sy;
+  rx2 *= sx;
+  ry2 *= sy;
+  rx3 *= sx;
+  ry3 *= sy;
+  rx4 *= sx;
+  ry4 *= sy;
+}
+
+void TRT::rotatePoint(int &x, int &y, float angle) {
+  int tempX = x;
+  x = tempX * cos(angle) - y * sin(angle);
+  y = tempX * sin(angle) + y * cos(angle);
+};
+
+void TRT::rotate() {
+  float deg;
+  cout << "Enter rotation angle: ";
+  cin >> deg;
+  float angle = deg * M_PI / 180;
+
+  // Rotate triangle vertices
+  rotatePoint(x1, y1, angle);
+  rotatePoint(x2, y2, angle);
+  rotatePoint(x3, y3, angle);
+
+  // Rotate rhombus vertices
+  rotatePoint(rx1, ry1, angle);
+  rotatePoint(rx2, ry2, angle);
+  rotatePoint(rx3, ry3, angle);
+  rotatePoint(rx4, ry4, angle);
+}
+
+void TRT::draw(int shapeType) {
+  int gdriver = DETECT, gmode;
+  initgraph(&gdriver, &gmode, NULL);
+  cleardevice();
+
+  if(shapeType == 1) { // Triangle
+    drawTriangle();
+  } else { // Rhombus
+    drawRhombus();
+  }
+}
+
+void TRT::drawTriangle() {
+  line(x1, y1, x2, y2);
+  line(x2, y2, x3, y3);
+  line(x3, y3, x1, y1);
+}
+
+void TRT::drawRhombus() {
+  line(rx1, ry1, rx2, ry2);
+  line(rx2, ry2, rx3, ry3);
+  line(rx3, ry3, rx4, ry4);
+  line(rx4, ry4, rx1, ry1);
+}
+
+int main() {
+  int option, shapeType;
+  cout << "=== Select shape to draw: ===\n1. Triangle\n2. Rhombus\nEnter your "
+          "choice: ";
+  cin >> shapeType;
+  TRT T;
+  T.draw(shapeType);
+  do {
+    cout << "\n1. Translation\n2. Scaling\n3. Rotation\n4. Exit: ";
+    cin >> option;
+    switch(option) {
+      case 1:
+        T.translate();
+        T.draw(shapeType);
+        break;
+      case 2:
+        T.scale();
+        T.draw(shapeType);
+        break;
+      case 3:
+        T.rotate();
+        T.draw(shapeType);
+        break;
+      case 4: break;
+      default: cout << "Invalid choice";
+    }
+  } while(option != 4);
+
+  closegraph();
+  return 0;
+}
